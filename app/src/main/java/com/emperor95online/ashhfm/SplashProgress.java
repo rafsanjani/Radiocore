9c73ece35f2928af36f27555d43acabacb8e1463
Aziz Rafsanjani <coded_raf@yahoo.com>
2019-03-10 10:31:48 +0800

    implement shared transition from splash screen to HomeActivity

--- app/src/main/java/com/emperor95online/ashhfm/SplashProgress.java
+++ app/src/main/java/com/emperor95online/ashhfm/SplashProgress.java
@@ -1,10 +1,14 @@
 package com.emperor95online.ashhfm;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 
 public class SplashProgress extends AppCompatActivity {
     @Override
@@ -12,12 +16,22 @@ public class SplashProgress extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_progress);
 
+        final ImageView backgroundImage = findViewById(R.id.backgroundImage);
+
         Handler handler = new Handler();
-        //just show a progress bar for a few seconds
+        //just show a progress bar for a few seconds and transition to the HaomeActivity
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashProgress.this, HomeActivity.class));
+                Pair pair = new Pair(backgroundImage, ViewCompat.getTransitionName(backgroundImage));
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(SplashProgress.this, pair);
+                    startActivity(new Intent(SplashProgress.this, HomeActivity.class), activityOptions.toBundle());
+                } else {
+                    startActivity(new Intent(SplashProgress.this, HomeActivity.class));
+                }
+
                 finish();
             }
         }, 3000);
