fbd29cc650ed84628021e6d0052e88ed025160f0
Aziz Rafsanjani <coded_raf@yahoo.com>
2019-05-28 11:38:43 +0000

    use Java 8 Lambdas

--- app/src/main/java/com/foreverrafs/starfm/data/NewsData.java
+++ app/src/main/java/com/foreverrafs/starfm/data/NewsData.java
@@ -5,7 +5,6 @@ import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -41,40 +40,32 @@ public class NewsData {
         RequestQueue queue = Volley.newRequestQueue(context);
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONArray newsArray = new JSONArray(response);
-                            for (int i = 0; i < newsArray.length(); i++) {
-                                JSONObject newsObject = newsArray.getJSONObject(i);
-                                String title = newsObject.getJSONObject("title").getString("rendered");
-                                String content = newsObject.getJSONObject("content").getString("rendered");
+                response -> {
+                    try {
+                        JSONArray newsArray = new JSONArray(response);
+                        for (int i = 0; i < newsArray.length(); i++) {
+                            JSONObject newsObject = newsArray.getJSONObject(i);
+                            String title = newsObject.getJSONObject("title").getString("rendered");
+                            String content = newsObject.getJSONObject("content").getString("rendered");
 
 //                                String image = "https://pbs.twimg.com/profile_images/425274582581264384/X3QXBN8C.jpeg";
-                                String image = "http://www.51allout.co.uk/wp-content/uploads/2012/02/Image-not-found.gif";
+                            String image = "http://www.51allout.co.uk/wp-content/uploads/2012/02/Image-not-found.gif";
 
-                                if (!newsObject.getJSONObject("_embedded").isNull("wp:featuredmedia")) {
-                                    image = newsObject.getJSONObject("_embedded")
-                                            .getJSONArray("wp:featuredmedia").getJSONObject(0)
-                                            .getString("source_url");
-                                }
-                                String date = newsObject.getString("date");
-
-                                newsList.add(new News(title, date.substring(0, date.indexOf("T")), image/*images.get(i)*/, content));
+                            if (!newsObject.getJSONObject("_embedded").isNull("wp:featuredmedia")) {
+                                image = newsObject.getJSONObject("_embedded")
+                                        .getJSONArray("wp:featuredmedia").getJSONObject(0)
+                                        .getString("source_url");
                             }
-                            newsFetchEventListener.onNewsFetched(newsList);
+                            String date = newsObject.getString("date");
 
-                        } catch (JSONException e) {
-                            Toast.makeText(context, "JSON Exception", Toast.LENGTH_SHORT).show();
+                            newsList.add(new News(title, date.substring(0, date.indexOf("T")), image/*images.get(i)*/, content));
                         }
+                        newsFetchEventListener.onNewsFetched(newsList);
+
+                    } catch (JSONException e) {
+                        Toast.makeText(context, "JSON Exception", Toast.LENGTH_SHORT).show();
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                newsFetchEventListener.onError(error);
-            }
-        });
+                }, error -> newsFetchEventListener.onError(error));
 
         queue.add(stringRequest);
     }
