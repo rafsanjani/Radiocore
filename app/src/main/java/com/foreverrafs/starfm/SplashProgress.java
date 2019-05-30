34a7472156de1c333c27e08fef569b3820df2bc8
Aziz Rafsanjani <coded_raf@yahoo.com>
2019-05-30 11:24:07 +0000

    disable transitions on splash

--- app/src/main/java/com/foreverrafs/starfm/SplashProgress.java
+++ app/src/main/java/com/foreverrafs/starfm/SplashProgress.java
@@ -1,14 +1,10 @@
 package com.foreverrafs.starfm;
 
-import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Pair;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ViewCompat;
 
 public class SplashProgress extends AppCompatActivity {
     @Override
@@ -16,18 +12,18 @@ public class SplashProgress extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_progress);
 
-        final ImageView backgroundImage = findViewById(R.id.backgroundImage);
+//        final ImageView backgroundImage = findViewById(R.id.backgroundImage);
 
         Handler handler = new Handler();
         //just show a progress bar for a few seconds and transition to the HomeActivity
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Pair pair = new Pair(backgroundImage, ViewCompat.getTransitionName(backgroundImage));
+//                Pair pair = new Pair(backgroundImage, ViewCompat.getTransitionName(backgroundImage));
 
                 if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                    ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(SplashProgress.this, pair);
-                    startActivity(new Intent(SplashProgress.this, HomeActivity.class), activityOptions.toBundle());
+//                    ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(SplashProgress.this, pair);
+                    startActivity(new Intent(SplashProgress.this, HomeActivity.class)/*, activityOptions.toBundle()*/);
                 } else {
                     startActivity(new Intent(SplashProgress.this, HomeActivity.class));
                 }
