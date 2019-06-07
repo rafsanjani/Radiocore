236616a1b6bdc372f753ec0e9d39ed62d7301db1
Aziz Rafsanjani <coded_raf@yahoo.com>
2019-06-07 14:48:59 +0000

    rename constant to radiocore

--- app/src/main/java/com/foreverrafs/starfm/util/Constants.java
+++ app/src/main/java/com/foreverrafs/starfm/util/Constants.java
@@ -3,7 +3,7 @@ package com.foreverrafs.starfm.util;
 // Created by Emperor95 on 3/5/2019.
 
 public class Constants {
-    public static final String DEBUG_TAG = "com.starfm";
+    public static final String DEBUG_TAG = "com.radiocore";
 
 
     //PERMISSION CONSTANTS
@@ -12,7 +12,7 @@ public class Constants {
 
     public static final String RESULT = "com.emperor95online.ashhfm.service.AudioStreamingService.REQUEST_PROCESSED";
     public static final String MESSAGE = "com.emperor95online.ashhfm.service.AudioStreamingService.AUDIO_STREAMING_STATUS";
-    //        public static final String STREAM_URL = "http://node-21.zeno.fm/sm3w0cp642quv?rj-ttl=5&rj-token=AAABauNwQ3G4lfvWvkyhRuTJ0_mgLB2oK-fWV41JG42CMMK1VQPH8Q";
+    //    public static final String STREAM_URL = "http://node-21.zeno.fm/sm3w0cp642quv?rj-ttl=5&rj-token=AAABauNwQ3G4lfvWvkyhRuTJ0_mgLB2oK-fWV41JG42CMMK1VQPH8Q";
     public static final String STREAM_URL = "http://media-ice.musicradio.com/CapitalGlasgowMP3";
     public static final String ACTION_PLAY = "com.emperor95Online.ashhfm.PLAY";
     public static final String ACTION_STOP = "com.emperor95Online.ashhfm.STOP";
@@ -24,13 +24,6 @@ public class Constants {
     public static final String STATUS_LOADING = "STATUS_LOADING";
     public static final String STATUS_DESTROYED = "STATUS_DESTROYED";
 
-
-    //APPLICATION SETTINGS
-    public static final String AUTOPLAY = "com.foreverrafs.starFM.autoplay";
-
     //Notification channel for Android 0
     public static final String NOTIFICATION_CHANNEL_ID = "com.emperor95Online.ashhfm";
-
-    //SETTINGS
-
 }
