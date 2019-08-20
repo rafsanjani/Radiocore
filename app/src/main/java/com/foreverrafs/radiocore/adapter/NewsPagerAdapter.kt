857fb957187add5ed34e903f6b7b3bc59bd4bfb8
Aziz Rafsanjani <coded_raf@yahoo.com>
2019-08-20 07:32:04 +0100

    track all untracked source files

---
+++ app/src/main/java/com/foreverrafs/radiocore/adapter/NewsPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.foreverrafs.radiocore.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.foreverrafs.radiocore.data.NewsData
+import com.foreverrafs.radiocore.fragment.NewsItemFragment
+
+/**
+ * Created by Rafsanjani on 7/24/2019
+ */
+class NewsPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getItem(position: Int): Fragment {
+        return NewsItemFragment.getInstance(position)
+    }
+
+    override fun getCount(): Int {
+        return NewsData.mNewsList!!.size
+    }
+}
---
+++ app/src/main/java/com/foreverrafs/radiocore/fragment/NewsItemFragment.kt
@@ -0,0 +1,62 @@
+package com.foreverrafs.radiocore.fragment
+
+import android.os.Bundle
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.foreverrafs.radiocore.R
+import com.foreverrafs.radiocore.data.NewsData
+import com.foreverrafs.radiocore.model.News
+import com.foreverrafs.radiocore.util.Constants
+import kotlinx.android.synthetic.main.fragment_news_item_detail.*
+import kotlinx.android.synthetic.main.news_detail_content.*
+import org.joda.time.format.DateTimeFormat
+
+class NewsItemFragment : Fragment() {
+
+    private var mNewsItem: News? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val bundle = arguments
+
+        if (bundle != null) {
+            mNewsItem = bundle.getParcelable(Constants.KEY_NEWS_ITEM)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_news_item_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val fmt = DateTimeFormat.forPattern("MMMM d, yyyy")
+        val datePretty = mNewsItem!!.date!!.toString(fmt)
+
+        tvHeadline.text = Html.fromHtml(mNewsItem?.headline)
+        tvDate.text = datePretty
+        tvContent.text = Html.fromHtml(mNewsItem?.content)
+        tvCategory.text = mNewsItem?.category
+
+        Glide.with(this)
+                .load(mNewsItem?.image)
+                .into(image!!)
+    }
+
+    companion object {
+
+        fun getInstance(position: Int): NewsItemFragment {
+            val newsItemAtPosition = NewsData.mNewsList!![position]
+
+            val fragmentNewsItem = NewsItemFragment()
+            val argument = Bundle()
+            argument.putParcelable(Constants.KEY_NEWS_ITEM, newsItemAtPosition)
+            fragmentNewsItem.arguments = argument
+            return fragmentNewsItem
+        }
+    }
+
+}
