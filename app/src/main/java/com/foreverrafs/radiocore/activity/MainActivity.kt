fee986759a4aa701370ec87127ddc61f60683c1c
Aziz Rafsanjani <coded_raf@yahoo.com>
2019-08-26 19:00:45 +0100

    move crashlytic initialization to MainActivity

--- app/src/main/java/com/foreverrafs/radiocore/activity/MainActivity.kt
+++ app/src/main/java/com/foreverrafs/radiocore/activity/MainActivity.kt
@@ -4,19 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.StrictMode
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
 import com.foreverrafs.radiocore.BuildConfig
 import com.foreverrafs.radiocore.R
+import io.fabric.sdk.android.Fabric
 
 /**
  * We just use this class to show a fake progress for 1/8th of a second and just proceeds to HomeActivity and dismiss it
  */
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         enableStrictMode()
-
+        setUpCrashlytics()
 
         Handler().postDelayed({
             startActivity(Intent(this, HomeActivity::class.java))
@@ -34,4 +38,13 @@ class MainActivity : AppCompatActivity() {
             StrictMode.setVmPolicy(policy)
         }
     }
+
+    private fun setUpCrashlytics() {
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, Crashlytics())
+            Log.i(TAG, "setUpCrashlytics: Enabled")
+        }
+    }
+
+
 }
